248944f8ebd89a0ed183d42970daaaae1f67f6db
ziqiong <ziqiong.22@ichat.sp.edu.sg>
2023-10-31 18:29:00 +0800

    Lab 3 Exercise 4

--- Test_employee_info.py
+++ Test_employee_info.py
@@ -9,8 +9,30 @@ employee_data = [
     {"name": "Peter", "age": 40, "department": "Sales", "salary": 60000}
 ]
 
+
+def test_get_employees_by_age_range():
+    lower_limit = 30
+    upper_limit = 40
+
+    result = employee_info.get_employees_by_age_range(lower_limit,upper_limit)
+    test = [{"name": "Chloe",  "age": 35, "department": "Engineering", "salary": 70000},
+    {"name": "Mike", "age": 32, "department": "Engineering", "salary": 65000}]
+
+    assert (result == test)
+
+
 def test_calculate_average_salary():
     result = round(employee_info.calculate_average_salary(),2)
     test = 60166.67
+
+    assert (result == test)
+
+
+def test_get_employees_by_dept():
+    department = "Marketing"
+
+    result = employee_info.get_employees_by_dept(department)
+    test = [{"name": "Jane", "age": 25, "department": "Marketing", "salary": 60000},
+    {"name": "Mary", "age": 23, "department": "Marketing", "salary": 56000}]
 
     assert (result == test)
